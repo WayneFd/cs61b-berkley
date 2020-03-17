ee9e2002b156ebf2b34c9604c376d500463ed931
Wayne <609025356@qq.com>
2020-03-17 12:03:21 +0800

    finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,90 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int L;
+    private int[][] world;
+    private WeightedQuickUnionUF QF;
+    private int opensite;
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N must be greater than zero");
+        } else {
+            L = N;
+            opensite = 0;
+            world = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    world[i][j] = 0;
+                }
+            }
+            QF = new WeightedQuickUnionUF(N * N);
+        }
+    }
+
+    // convert the 2d Coordinate to the id in QF
+    private int coorTrans(int row, int col) {
+        return row * L + col;
+    }
+
+    // to make connect when neighbors are open
+    private void makeConnect(int row, int col) {
+        int target = coorTrans(row, col);
+        if (isOpen(row - 1, col)) {
+            QF.union(target, coorTrans(row - 1, col));
+        }
+        if (isOpen(row + 1, col)) {
+            QF.union(target, coorTrans(row + 1, col));
+        }
+        if (isOpen(row, col - 1)) {
+            QF.union(target, coorTrans(row, col - 1));
+        }
+        if (isOpen(row, col + 1)) {
+            QF.union(target, coorTrans(row, col + 1));
+        }
+
+    }
+    public boolean isOpen(int row, int col) {
+        if (row == -1 || row == L || col == -1 || col == L) {
+            return false;
+        } else {
+            return world[row][col] == 1;
+        }
+    }
+
+    public void open(int row, int col) {
+        if (!isOpen(row, col)) {
+            world[row][col] = 1;
+            opensite += 1;
+            makeConnect(row, col);
+
+
+        }
+    }
+
+    public boolean isFull(int row, int col) {
+        int target = coorTrans(row, col);
+        for (int j = 0; j < L; j++) {
+            if (isOpen(0, j)) {
+                if (QF.connected(target, j)) {
+                    return true;
+                }
+            }
+        }
+        return  false;
+    }
+
+    public int numberOfOpenSites() {
+        return opensite;
+    }
+
+    public boolean percolates() {
+        for (int j = 0; j < L; j++) {
+            if (isFull(L - 1, j)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
+
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,49 @@
 package hw2;
 
+//import java.sql.Time;
+//import java.util.Map;
+
 public class PercolationStats {
+    private int times;
+    private double[] opened;
+    private Percolation expe;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        times = T;
+        opened = new double[T];
+        for (int i = 0; i < T; i++) {
+            expe  = pf.make(N);
+            while (!expe.percolates()) {
+                int row = (int) (Math.random() * N);
+                int col = (int) (Math.random() * N);
+                expe.open(row, col);
+            }
+            opened[i] = expe.numberOfOpenSites();
+        }
+    }
+    public double mean() {
+        double mean = 0.0;
+        for (int i = 0; i < opened.length; i++) {
+            mean += opened[i];
+        }
+        return mean / times;
+    }
+
+    public double stdddev() {
+        double mean = mean();
+        double sigma = 0.0;
+        for (int i = 0; i < opened.length; i++) {
+            sigma += Math.pow((opened[i] - mean), 2);
+        }
+        return sigma / (times - 1);
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stdddev() / Math.sqrt(times);
+    }
+
+    public double confidenceHige() {
+        return mean() + 1.96 * stdddev() / Math.sqrt(times);
+    }
 
 }
